e2aeac702f0e8a68f669b6be0b5ad4674a9e87f5
YasminAmana <rhp788@alumni.ku.dk>
2023-03-14 11:16:44 +0100

    add parameters

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,4 +1,24 @@
-def square(x):
+class inauguralproject:
+
+    def __init__(self):
+    # a. create namespaces
+        par = self.par = SimpleNamespace()
+
+    # add preferences
+        par.p = 2
+        par.v = 0.001
+        par.omega = 0.5 
+
+    # add household production
+        par.alpha = 0.5
+        par.sigma = 1
+    
+    # add wages
+        par.wM = 1
+        par.wF = 1
+        
+
+    def func_marketgoods():
     """ square numpy array
     
     Args:
