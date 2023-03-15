379f382248fc24588cba8412949946ef78bf0f3e
YasminAmana <rhp788@alumni.ku.dk>
2023-03-15 16:46:30 +0100

    question 1

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,58 +1,74 @@
 from types import SimpleNamespace
+
 import numpy as np
+from scipy import optimize
+
+import pandas as pd 
+import matplotlib.pyplot as plt
 
 class inauguralproject:
 
     def __init__(self):
-    # a. create namespaces
+        """ setup model """
+
+        # a. create namespaces
         par = self.par = SimpleNamespace()
         sol = self.sol = SimpleNamespace()
 
-    # add preferences
-        par.rho = 2
-        par.v = 0.001
+        # b. preferences
+        par.rho = 2.0
+        par.nu = 0.001
+        par.epsilon = 1.0
         par.omega = 0.5 
-        par.epsilon = 1
-       
 
-    # add household production
+        # c. household production
         par.alpha = 0.5
-        par.sigma = 1
-    
-    # add wages
-        par.wM = 1
-        par.wF = 1
+        par.sigma = 1.0
+
+        # d. wages
+        par.wM = 1.0
+        par.wF = 1.0
         par.wF_vec = np.linspace(0.8,1.2,5)
 
-        
-    #define utility functions
-    def calc (self,LM,HM,LF,HF):
-        
-        par = self.par
+        # e. targets
+        par.beta0_target = 0.4
+        par.beta1_target = -0.1
 
-        # Consumption 
+        # f. solution
+        sol.LM_vec = np.zeros(par.wF_vec.size)
+        sol.HM_vec = np.zeros(par.wF_vec.size)
+        sol.LF_vec = np.zeros(par.wF_vec.size)
+        sol.HF_vec = np.zeros(par.wF_vec.size)
+
+        sol.beta0 = np.nan
+        sol.beta1 = np.nan
+
+    def calc_utility(self,LM,HM,LF,HF):
+        """ calculate utility """
+
+        par = self.par
+        sol = self.sol
+
+        # a. consumption of market goods
         C = par.wM*LM + par.wF*LF
 
-        # Home production
+        # b. home production
         H = HM**(1-par.alpha)*HF**par.alpha
 
-        # Total consumption
+        # c. total consumption utility
         Q = C**par.omega*H**(1-par.omega)
+        utility = np.fmax(Q,1e-8)**(1-par.rho)/(1-par.rho)
 
-        #Utility of work
-        utility = (Q**(1-par.p))/(1-par.p)
-
-        #Utility-cost of work
-        TM = LM*HM
-        TF = LF*HF
-        cost = par.v*(TM**(1+1/par.epsilon)/(1+1/par.epsilon)+TF**(1+1/par.epsilon)/(1+1/par.epsilon))
+        # d. disutlity of work
+        epsilon_ = 1+1/par.epsilon
+        TM = LM+HM
+        TF = LF+HF
+        disutility = par.nu*(TM**epsilon_/epsilon_+TF**epsilon_/epsilon_)
         
-        #Total utility
-        tot_utility = utility - cost
-        return tot_utility 
+        return utility - disutility
 
-#im not sure about this part we might look at that again
-def solve_discrete(self,do_print=False):
+    def solve_discrete(self,do_print=False):
+        """ solve model discretely """
         
         par = self.par
         sol = self.sol
@@ -67,6 +83,50 @@ def solve_discrete(self,do_print=False):
         LF = LF.ravel()
         HF = HF.ravel()
 
-#until here im not sure if its correct (becuase its just a copy of his code)
+        # b. calculate utility
+        u = self.calc_utility(LM,HM,LF,HF)
+    
+        # c. set to minus infinity if constraint is broken
+        I = (LM+HM > 24) | (LF+HF > 24) # | is "or"
+        u[I] = -np.inf
+    
+        # d. find maximizing argument
+        j = np.argmax(u)
+        
+        opt.LM = LM[j]
+        opt.HM = HM[j]
+        opt.LF = LF[j]
+        opt.HF = HF[j]
 
+        # e. print
+        if do_print:
+            for k,v in opt.__dict__.items():
+                print(f'{k} = {v:6.4f}')
 
+        return opt
+
+    def solve(self,do_print=False):
+        """ solve model continously """
+
+        pass    
+
+    def solve_wF_vec(self,discrete=False):
+        """ solve model for vector of female wages """
+
+        pass
+
+    def run_regression(self):
+        """ run regression """
+
+        par = self.par
+        sol = self.sol
+
+        x = np.log(par.wF_vec)
+        y = np.log(sol.HF_vec/sol.HM_vec)
+        A = np.vstack([np.ones(x.size),x]).T
+        sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0]
+    
+    def estimate(self,alpha=None,sigma=None):
+        """ estimate alpha and sigma """
+
+        pass
