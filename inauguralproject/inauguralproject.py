0d222f40d20d51f7c4a21f2e242547688fd0dba1
YasminAmana <rhp788@alumni.ku.dk>
2023-03-26 10:10:17 +0200

    ??

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -203,9 +203,6 @@ class inauguralproject:
                 var = (par.beta0_target-sol.beta0)**2 + (par.beta1_target-sol.beta1)**2
                 print(var)
 
-                alpha = alpha.astype(int)
-                sigma = sigma.astype(int)
-
                 if min_var is np.nan:
                     A = par.alpha
                     S = par.sigma 
