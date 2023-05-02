eb76055e9e1130f42d73bf8bdc5455bff456bd31
ToreLovik <dpv897@alumni.ku.dk>
2023-05-02 10:51:38 +0200

    Started on a class

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,7 +1,33 @@
 from scipy import optimize
+from types import SimpleNamespace
 
+import numpy as np
+from scipy import optimize
+
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+class model:
+
+    def __inits__(self):
+
+        # Namespaces
+        par = self.par = SimpleNamespace()
+        sol = self.sol = SimpleNamespace()
+
+        # Input parameters
+        par.K1 = 2000
+        par.K2 = 1000
+        par.L1 = 2000
+        par.L2 = 1000
+
+
+
+
+
+""""
 def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
+     Example function. Solve for steady state k. 
 
     Args:
         c (float): costs
@@ -10,7 +36,7 @@ def solve_ss(alpha, c):
     Returns:
         result (RootResults): the solution represented as a RootResults object.
 
-    """ 
+     
     
     # a. Objective function, depends on k (endogenous) and c (exogenous).
     f = lambda k: k**alpha - c
@@ -19,4 +45,4 @@ def solve_ss(alpha, c):
     #. b. call root finder to find kss.
     result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
     
-    return result
+    return result """
