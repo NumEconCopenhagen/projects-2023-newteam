00f5f0c212a327b3a9951d112cd756115f228dd4
ToreLovik <dpv897@alumni.ku.dk>
2023-05-02 11:11:50 +0200

    initial class setup

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -16,10 +16,21 @@ class model:
         sol = self.sol = SimpleNamespace()
 
         # Input parameters
+        # Capital and labor
         par.K1 = 2000
         par.K2 = 1000
         par.L1 = 2000
         par.L2 = 1000
+
+        # Price of labor and capital
+        par.w1 = 2
+        par.w2 = 1
+        par.r1 = 1
+        par.r2 = 2
+
+
+
+
 
 
 
